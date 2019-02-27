1727e79a43b12efa017f085506ec6f08005bb148
unknown <poetrageranting@gmail.com>
2019-02-26 20:08:21 -0800

    mengapus komentar login

--- application/views/login.php
+++ application/views/login.php
@@ -5,28 +5,19 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="<?= base_url().'assets/bower_components/bootstrap/dist/css/bootstrap.min.css' ?>">
-  <!-- Font Awesome -->
+<link rel="stylesheet" href="<?= base_url().'assets/bower_components/bootstrap/dist/css/bootstrap.min.css' ?>">
   <link rel="stylesheet" href="<?= base_url().'assets/bower_components/font-awesome/css/font-awesome.min.css' ?>">
-  <!-- Ionicons -->
   <link rel="stylesheet" href="<?= base_url().'assets/bower_components/Ionicons/css/ionicons.min.css' ?>">
-  <!-- Theme style -->
   <link rel="stylesheet" href="<?= base_url().'assets/dist/css/AdminLTE.min.css' ?>">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="<?= base_url().'assets/dist/css/skins/_all-skins.min.css' ?>">
-  <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="<?= base_url().'assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css' ?>">
+ <link rel="stylesheet" href="<?= base_url().'assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css' ?>">
 
-  <!-- DataTables -->
   <link rel="stylesheet" href="<?= base_url().'assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css' ?>">
 
   <link rel="stylesheet" href="<?= base_url().'assets/bower_components/select2/dist/css/select2.min.css'?>">
   <link rel="stylesheet" href="<?= base_url().'assets/plugins/iCheck/square/blue.css'?>">
   <meta charset="utf-8">
   <title>LICENSE | Log in</title>
-  <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
 </head>
